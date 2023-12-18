eaf61782d9d7d7ea23cb2c24b9cdec0c695ecf33
Eduardo Schelive <eduardoschelive@gmail.com>
2023-12-18 10:04:52 -0300

    🔨 refactor(server): cleanup code

--- server/src/main/java/com/xlr8code/server/authentication/controller/AuthenticationController.java
+++ server/src/main/java/com/xlr8code/server/authentication/controller/AuthenticationController.java
@@ -1,4 +1,11 @@
 package com.xlr8code.server.authentication.controller;
 
+import com.xlr8code.server.authentication.utils.Endpoint;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(Endpoint.AUTHENTICATION)
 public class AuthenticationController {
+
 }
--- server/src/main/java/com/xlr8code/server/authentication/utils/RoleEndpoints.java
+++ server/src/main/java/com/xlr8code/server/authentication/utils/RoleEndpoints.java
@@ -1,25 +1,21 @@
-package com.xlr8code.server.user.utils;
+package com.xlr8code.server.authentication.utils;
 
-import lombok.RequiredArgsConstructor;
+import com.xlr8code.server.user.utils.UserRole;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.EnumMap;
 import java.util.Map;
 
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RoleEndpoints {
 
-
-
     public static Map<UserRole, String[]> getRoleEndpoints() {
-
         var endpoints = new EnumMap<UserRole, String[]>(UserRole.class);
 
-        endpoints.put(UserRole.MEMBER, new String[]{"/api/member"});
-        endpoints.put(UserRole.ADMIN, new String[]{"/api/admin"});
-
+        endpoints.put(UserRole.MEMBER, new String[]{Endpoint.FAKE});
 
         return endpoints;
-
     }
 
 }
