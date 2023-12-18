5aec529a3aa353549e2fd8346bff3c7e4f05761a
Eduardo Schelive <eduardoschelive@gmail.com>
2023-12-18 10:04:52 -0300

    ✨ feat(server): configure security and roles
    configure the bases of security and application roles

---
+++ server/src/main/java/com/xlr8code/server/common/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.xlr8code.server.common.service;
+
+import com.xlr8code.server.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        var user = this.userService.findByLogin(login);
+        return user.toUserDetails();
+    }
+
+}
