119510d7f32bf8c7338850dca2c629ab955c4220
Eduardo Schelive <eduardoschelive@gmail.com>
2023-12-18 16:41:29 -0300

    ✨ feat: add logging structure

--- server/src/main/java/com/xlr8code/server/user/listener/CreateRolesListener.java
+++ server/src/main/java/com/xlr8code/server/user/listener/CreateRolesListener.java
@@ -2,16 +2,22 @@ package com.xlr8code.server.user.listener;
 
 import com.xlr8code.server.user.service.RoleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CreateRolesListener implements ApplicationListener<ContextRefreshedEvent> {
 
     private final RoleService roleService;
+
 
     @Override
     public void onApplicationEvent(@Nullable ContextRefreshedEvent event) {
