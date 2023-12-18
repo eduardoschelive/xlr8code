5b6f8e9a062558b82eef3c922c5810fb97443243
Eduardo Schelive <eduardoschelive@gmail.com>
2023-12-18 10:04:52 -0300

    🔨 refactor(server): cleanup code

--- server/src/main/java/com/xlr8code/server/authentication/utils/Endpoint.java
+++ server/src/main/java/com/xlr8code/server/authentication/utils/Endpoint.java
@@ -1,4 +1,13 @@
 package com.xlr8code.server.authentication.utils;
 
-public class Endpoints {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Endpoint {
+
+    private static final String PREFIX = "/api";
+    public static final String AUTHENTICATION = PREFIX + "/authentication";
+    public static final String FAKE = PREFIX + "/fake";
+
 }
