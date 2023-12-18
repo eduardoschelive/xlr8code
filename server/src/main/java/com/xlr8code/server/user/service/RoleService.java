afb260885b2ca32f1858cc7375d81b1d48ff2165
Eduardo Schelive <eduardoschelive@gmail.com>
2023-12-18 14:08:42 -0300

    📚 docs(server): remove unnecessary docs

--- server/src/main/java/com/xlr8code/server/user/service/RoleService.java
+++ server/src/main/java/com/xlr8code/server/user/service/RoleService.java
@@ -13,12 +13,6 @@ public class RoleService {
 
     private final RoleRepository roleRepository;
 
-    /**
-     * <p>
-     *     This method is called when the application is started.
-     *     It creates the roles in the database if they don't exist.
-     * </p>
-     */
     public void createRoles() {
         var roles = Arrays.stream(UserRole.values()).map(UserRole::toRole).toList();
         this.roleRepository.saveAll(roles);
