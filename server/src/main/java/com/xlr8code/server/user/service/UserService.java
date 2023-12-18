81bf57e9cb63afdcd72c7a0683500860ec2ceebd
Eduardo Schelive <eduardoschelive@gmail.com>
2023-12-18 10:04:52 -0300

    ✨ feat(server): remove token account activation

--- server/src/main/java/com/xlr8code/server/user/service/UserService.java
+++ server/src/main/java/com/xlr8code/server/user/service/UserService.java
@@ -1,14 +1,11 @@
 package com.xlr8code.server.user.service;
 
-import com.xlr8code.server.authentication.events.OnSignUpCompleteEvent;
 import com.xlr8code.server.common.exception.ApplicationError;
 import com.xlr8code.server.user.entity.User;
 import com.xlr8code.server.user.exception.UserErrorCode;
 import com.xlr8code.server.user.repository.UserRepository;
 import com.xlr8code.server.user.utils.UserRole;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +17,8 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public User create(String username, String email, String password, HttpServletRequest request) throws ApplicationError {
+    public User create(String username, String email, String password) throws ApplicationError {
         var userOptional = this.userRepository.findUserByUsernameOrEmailIgnoreCase(username, email);
 
         if (userOptional.isPresent()) {
@@ -38,18 +34,11 @@ public class UserService {
                 .roles(roles)
                 .build();
 
-        var newUser = this.userRepository.save(user);
-        this.applicationEventPublisher.publishEvent(new OnSignUpCompleteEvent(newUser, request.getContextPath()));
-
-        return newUser;
+        return this.userRepository.save(user);
     }
 
     public User findByLogin(String login) {
         return this.userRepository.findUserByLogin(login);
-    }
-
-    public User update(User user) {
-        return this.userRepository.save(user);
     }
 
 }
