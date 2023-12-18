9790b81f7764f72018d69d99109eb76efd77cc57
Eduardo Schelive <eduardoschelive@gmail.com>
2023-12-18 10:04:51 -0300

    ✨ feat(server): add param annotation

--- server/src/main/java/com/xlr8code/server/common/listener/ContextRefreshedEventListener.java
+++ server/src/main/java/com/xlr8code/server/common/listener/ContextRefreshedEventListener.java
@@ -2,18 +2,19 @@ package com.xlr8code.server.common.listener;
 
 import com.xlr8code.server.user.service.RoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class DefaultUserRolesStartup {
+public class ContextRefreshedEventListener implements ApplicationListener<ContextRefreshedEvent> {
 
     private final RoleService roleService;
 
-    @EventListener(ContextRefreshedEvent.class)
-    public void onContextRefreshed() {
+    @Override
+    public void onApplicationEvent(@NonNull ContextRefreshedEvent event) {
         this.roleService.createDefaultRoles();
     }
 }
