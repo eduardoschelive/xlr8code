e563a8f0ec1b56a85a97b19ce04db63da16de4bf
Eduardo Schelive <eduardoschelive@gmail.com>
2023-12-29 17:37:52 -0300

    🐛 fix(i18n): fix internationalization header
    fixed internationalization header to accept lower case values

--- server/src/main/java/com/xlr8code/server/authentication/exception/InvalidTokenException.java
+++ server/src/main/java/com/xlr8code/server/authentication/exception/InvalidTokenException.java
@@ -3,15 +3,15 @@ package com.xlr8code.server.authentication.exception;
 import com.xlr8code.server.common.exception.ApplicationException;
 import org.springframework.http.HttpStatus;
 
-public class InvalidToken extends ApplicationException {
+public class InvalidTokenException extends ApplicationException {
 
-    public InvalidToken() {
+    public InvalidTokenException() {
         super("INVALID_TOKEN");
     }
 
     @Override
     public String getMessageIdentifier() {
-        return "error.authentication.invalid_token";
+        return "authentication.error.invalid_token";
     }
 
     @Override
--- server/src/main/java/com/xlr8code/server/common/utils/UUIDUtils.java
+++ server/src/main/java/com/xlr8code/server/common/utils/UUIDUtils.java
@@ -1,4 +1,20 @@
 package com.xlr8code.server.common.utils;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UUIDUtils {
+
+    public static Optional<UUID> fromString(String string) {
+        try {
+            return Optional.of(UUID.fromString(string));
+        } catch (IllegalArgumentException e) {
+            return Optional.empty();
+        }
+    }
+
 }
