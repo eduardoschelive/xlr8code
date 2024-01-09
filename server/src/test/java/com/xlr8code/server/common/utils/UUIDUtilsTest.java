2dc27acf8ab92eabfa8a6cc8f2ae3bae4679cf2d
Eduardo Schelive <eduardoschelive@gmail.com>
2024-01-09 17:08:38 -0300

    🚨 test(common): test random utils class

--- server/src/test/java/com/xlr8code/server/common/utils/UUIDUtilsTest.java
+++ server/src/test/java/com/xlr8code/server/common/utils/UUIDUtilsTest.java
@@ -1,11 +1,30 @@
 package com.xlr8code.server.common.utils;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class UUIDUtilsTest {
 
     @Test
-    void fromString() {
+    void it_should_return_empty_when_invalid_uuid() {
+        var invalidUUID = "invalid";
+
+        var uuid = UUIDUtils.convertFromString(invalidUUID);
+
+        assertTrue(uuid.isEmpty());
     }
+
+    @Test
+    void it_should_return_uuid_when_valid_uuid() {
+        var validUUID = "123e4567-e89b-12d3-a456-426614174000";
+
+        var uuid = UUIDUtils.convertFromString(validUUID);
+
+        assertTrue(uuid.isPresent());
+    }
+
 }
