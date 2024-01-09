52312f501127a2d84190513fbdadae0cf816789f
Eduardo Schelive <eduardoschelive@gmail.com>
2024-01-09 17:07:52 -0300

    🧹 chore: add common to better commits config

--- server/src/test/java/com/xlr8code/server/common/utils/RandomUtilsTest.java
+++ server/src/test/java/com/xlr8code/server/common/utils/RandomUtilsTest.java
@@ -1,20 +1,41 @@
 package com.xlr8code.server.common.utils;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class RandomUtilsTest {
 
     @Test
-    void generate() {
+    void it_should_generate_correct_length() {
+        var length = 6;
+
+        var randomCode = RandomUtils.generate(length);
+
+        assertEquals(length, randomCode.length());
     }
 
     @Test
-    void testGenerate() {
+    void it_should_generate_only_allowed_characters() {
+        var allowedCharacters = "0123456789!)%*";
+        var matchRegex = "[" + allowedCharacters + "]+";
+
+        var randomCode = RandomUtils.generate(6, allowedCharacters);
+
+        assertTrue(randomCode.matches(matchRegex));
     }
 
     @Test
-    void generateAlphanumeric() {
+    void it_should_generate_alphanumeric_code() {
+        var length = 6;
+        var matchRegex = "[0-9A-Za-z]+";
+
+        var randomCode = RandomUtils.generateAlphanumeric(length);
+
+        assertTrue(randomCode.matches(matchRegex));
     }
 }
---
+++ server/src/test/java/com/xlr8code/server/common/utils/UUIDUtilsTest.java
@@ -0,0 +1,11 @@
+package com.xlr8code.server.common.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UUIDUtilsTest {
+
+    @Test
+    void fromString() {
+    }
+}
