da445159c02c8d30faa18bdd4c54019e4dc1a77c
Eduardo Schelive <eduardoschelive@gmail.com>
2024-01-09 17:09:15 -0300

    🚨 test(common): test date time utils

--- server/src/test/java/com/xlr8code/server/common/utils/DateTimeUtilsTest.java
+++ server/src/test/java/com/xlr8code/server/common/utils/DateTimeUtilsTest.java
@@ -1,6 +1,33 @@
 package com.xlr8code.server.common.utils;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.time.temporal.ChronoUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DateTimeUtilsTest {
-  
+
+    @Test
+    void it_should_calculate_expires_at() {
+        long expirationTime = 1;
+
+        var expiresAt = DateTimeUtils.calculateExpiresAt(expirationTime, ChronoUnit.SECONDS);
+
+        assertNotNull(expiresAt);
+    }
+
+    @Test
+    void it_should_check_if_expired() {
+        var expiresAt = DateTimeUtils.calculateExpiresAt(-1, ChronoUnit.SECONDS);
+
+        var isExpired = DateTimeUtils.isExpired(expiresAt);
+
+        assertTrue(isExpired);
+    }
+
+
 }
