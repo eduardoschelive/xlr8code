acc8c09c0d5bca81c56efa785b12e89cabb9857d
Eduardo Schelive <eduardoschelive@gmail.com>
2024-01-12 11:52:01 -0300

    🚨 test(auth): test user activation code

---
+++ server/src/test/java/com/xlr8code/server/authentication/service/UserActivationCodeServiceTest.java
@@ -0,0 +1,19 @@
+package com.xlr8code.server.authentication.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserActivationCodeServiceTest {
+
+    @Test
+    void generate() {
+    }
+
+    @Test
+    void validate() {
+    }
+
+    @Test
+    void removeAllFromUser() {
+    }
+}
