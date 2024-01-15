5ed69de5a6db28117b8fbe62444b42377fb7bd7b
Eduardo Schelive <eduardoschelive@gmail.com>
2024-01-15 09:33:30 -0300

    📚 docs(user): change return type of method

--- server/src/test/java/com/xlr8code/server/authentication/service/CustomUserDetailsServiceTest.java
+++ server/src/test/java/com/xlr8code/server/authentication/service/CustomUserDetailsServiceTest.java
@@ -1,15 +1,99 @@
 package com.xlr8code.server.authentication.service;
 
-import org.junit.jupiter.api.Test;
+import com.xlr8code.server.authentication.exception.IncorrectUsernameOrPasswordException;
+import com.xlr8code.server.common.utils.Language;
+import com.xlr8code.server.common.utils.Theme;
+import com.xlr8code.server.user.dto.CreateUserDTO;
+import com.xlr8code.server.user.exception.UserNotFoundException;
+import com.xlr8code.server.user.repository.UserRepository;
+import com.xlr8code.server.user.service.UserService;
+import com.xlr8code.server.user.utils.UserRole;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CustomUserDetailsServiceTest {
 
-    @Test
-    void loadUserByUsername() {
+    public static final String USERNAME = "test";
+    public static final String MAIL = "test@test.com";
+
+    public static UUID createdUserId;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @BeforeAll
+    static void setUp(@Autowired UserService userService) {
+        var user = new CreateUserDTO(
+                USERNAME,
+                MAIL,
+                "test",
+                Set.of(UserRole.MEMBER.toRole()),
+                Theme.SYSTEM,
+                Language.AMERICAN_ENGLISH,
+                null,
+                false);
+
+        var createdUser = userService.create(user);
+        createdUserId = createdUser.getId();
+    }
+
+    @AfterAll
+    static void tearDownAll(@Autowired UserRepository userRepository) {
+        userRepository.deleteAll();
     }
 
     @Test
-    void loadUserById() {
+    void it_should_load_user_by_username() {
+        var user = this.customUserDetailsService.loadUserByUsername(USERNAME);
+
+        assertNotNull(user);
     }
+
+    @Test
+    void it_should_load_user_by_email_as_username() {
+        var user = this.customUserDetailsService.loadUserByUsername(MAIL);
+
+        assertNotNull(user);
+    }
+
+    @Test
+    void it_should_load_user_by_id() {
+        var user = this.customUserDetailsService.loadUserById(createdUserId);
+
+        assertNotNull(user);
+    }
+
+    @Test
+    void it_should_throw_exception_when_user_not_found_by_id() {
+        UUID uuid = UUID.randomUUID();
+
+        Executable executable = () -> this.customUserDetailsService.loadUserById(uuid);
+
+        assertThrows(UserNotFoundException.class, executable);
+    }
+
+    @Test
+    void it_should_throw_exception_when_user_not_found_by_username() {
+        Executable executable = () -> this.customUserDetailsService.loadUserByUsername("not_found");
+
+        assertThrows(IncorrectUsernameOrPasswordException.class, executable);
+    }
+
+    @Test
+    void it_should_throw_exception_when_user_not_found_by_email() {
+        Executable executable = () -> this.customUserDetailsService.loadUserByUsername("not_test");
+
+        assertThrows(IncorrectUsernameOrPasswordException.class, executable);
+    }
+
 }
