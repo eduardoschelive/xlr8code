af2373b63cc3417fc54dc9efa03bbd24c6b73ea9
Eduardo Schelive <eduardoschelive@gmail.com>
2024-01-16 14:43:37 -0300

    🚨 test(auth): test AccessTokenService

---
+++ server/src/test/java/com/xlr8code/server/authentication/service/UserSessionServiceTest.java
@@ -0,0 +1,32 @@
+package com.xlr8code.server.authentication.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserSessionServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void validateSessionToken() {
+    }
+
+    @Test
+    void refresh() {
+    }
+
+    @Test
+    void end() {
+    }
+
+    @Test
+    void endAllFromUser() {
+    }
+
+    @Test
+    void getSessionDuration() {
+    }
+}
