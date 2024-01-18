66c3248e639310a22b10b5ba1c74a10c2520d0de
Eduardo Schelive <eduardoschelive@gmail.com>
2024-01-18 15:00:36 -0300

    🚨 test: create JsonTestUtils
    created JsonTestUtils, this class will be used to convert objects in a json string to send in controllers test

--- server/src/test/java/com/xlr8code/server/utils/JsonTestUtils.java
+++ server/src/test/java/com/xlr8code/server/utils/JsonTestUtils.java
@@ -1,4 +1,50 @@
 package com.xlr8code.server.utils;
 
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.xlr8code.server.common.serialization.deserializer.LanguageDeserializer;
+import com.xlr8code.server.common.serialization.deserializer.ThemeDeserializer;
+import com.xlr8code.server.common.serialization.serializer.LanguageSerializer;
+import com.xlr8code.server.common.serialization.serializer.ThemeSerializer;
+import com.xlr8code.server.common.utils.Language;
+import com.xlr8code.server.common.utils.Theme;
+
 public class JsonTestUtils {
+
+    private static <T> Module createSimpleModule(Class<T> clazz, JsonSerializer<T> serializer, JsonDeserializer<T> deserializer) {
+        var module = new SimpleModule();
+        module.addSerializer(clazz, serializer);
+        module.addDeserializer(clazz, deserializer);
+        return module;
+    }
+
+    private static <T> void registerNewModule(ObjectMapper objectMapper, Class<T> clazz, JsonSerializer<T> serializer, JsonDeserializer<T> deserializer) {
+        objectMapper.registerModule(createSimpleModule(clazz, serializer, deserializer));
+    }
+
+    private static void registerModules(ObjectMapper objectMapper) {
+        registerNewModule(objectMapper, Theme.class, new ThemeSerializer(), new ThemeDeserializer());
+        registerNewModule(objectMapper, Language.class, new LanguageSerializer(), new LanguageDeserializer());
+    }
+
+    private static ObjectMapper getObjectMapper() {
+        var objectMapper = new ObjectMapper();
+
+        registerModules(objectMapper);
+
+        return objectMapper;
+    }
+
+    public static String asJsonString(final Object obj) {
+        var objectMapper = getObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
