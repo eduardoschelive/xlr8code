dcf2951769ad6b793f4c5d24ff3824d0d8bae103
Eduardo Schelive <eduardoschelive@gmail.com>
2024-03-18 11:05:20 -0300

    🔨 refactor(series): refactor series put endpoint

--- server/src/main/java/com/xlr8code/server/series/service/I18nSeriesService.java
+++ server/src/main/java/com/xlr8code/server/series/service/I18nSeriesService.java
@@ -3,7 +3,6 @@ package com.xlr8code.server.series.service;
 import com.xlr8code.server.common.exception.DuplicateSlugInLanguagesException;
 import com.xlr8code.server.common.exception.SlugAlreadyExistsException;
 import com.xlr8code.server.series.dto.SeriesLanguageDTO;
-import com.xlr8code.server.series.entity.I18nSeries;
 import com.xlr8code.server.series.repository.I18nSeriesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,63 +16,56 @@ public class I18nSeriesService {
 
     private final I18nSeriesRepository i18nSeriesRepository;
 
-    /**
-     * @param i18nSeries the list of series to be validated
-     * @throws DuplicateSlugInLanguagesException if the slug already exists in the list
-     */
-    public void validateDuplicateSlugs(Set<I18nSeries> i18nSeries) {
-        Set<String> slugSet = new HashSet<>();
-        for (I18nSeries series : i18nSeries) {
-            var slug = series.getSlug();
-            if (!slugSet.add(slug)) {
-                throw new DuplicateSlugInLanguagesException(slug);
-            }
-        }
-    }
-
-    /**
-     * @param i18nSeries the list of series to be validated
-     * @throws SlugAlreadyExistsException if the slug already exists in the database
-     */
     @Transactional(readOnly = true)
-    public void validateSlugsInDatabase(Set<I18nSeries> i18nSeries) {
-        for (I18nSeries series : i18nSeries) {
-            var slug = series.getSlug();
-            var id =  series.getSeries().getId();
-            boolean existsBySlugAndSeriesIdNot = id == null ? i18nSeriesRepository.existsBySlug(slug) : i18nSeriesRepository.existsBySlugAndSeriesIdNot(slug, id);
-            if (existsBySlugAndSeriesIdNot){
-                throw new SlugAlreadyExistsException(slug);
-            }
-        }
-    }
-
-    /**
-     * @param i18nSeries the list of series to be validated
-     * @throws DuplicateSlugInLanguagesException if the slug already exists in the list
-     * @throws SlugAlreadyExistsException        if the slug already exists in the database
-     */
-    @Transactional(readOnly = true)
-    public void validateSlugInList(Set<I18nSeries> i18nSeries) {
+    public void validateSlugInList(Collection<SeriesLanguageDTO> i18nSeries) {
         this.validateDuplicateSlugs(i18nSeries);
-        this.validateSlugsInDatabase(i18nSeries);
+        List<String> slugs = i18nSeries.stream()
+                .map(SeriesLanguageDTO::slug)
+                .toList();
+        validateSlugInCollection(slugs);
     }
 
-    public void validateDuplicateSlugInCollection(Collection<SeriesLanguageDTO> languages, UUID seriesId) {
-        Set<String> slugSet = new HashSet<>();
-        for (var language : languages) {
-            var slug = language.slug();
-            if (!slugSet.add(slug)) {
-                throw new DuplicateSlugInLanguagesException(slug);
-            }
-            this.validateDuplicateSlugWithOwner(slug, seriesId);
-        }
+    @Transactional(readOnly = true)
+    public void validateSlugInListWithOwner(Collection<SeriesLanguageDTO> i18nSeries, UUID seriesId) {
+        this.validateDuplicateSlugs(i18nSeries);
+        List<String> slugs = i18nSeries.stream()
+                .map(SeriesLanguageDTO::slug)
+                .toList();
+        validateSlugWithOwnerInCollection(slugs, seriesId);
     }
 
-    public void validateDuplicateSlugWithOwner(String slug, UUID seriesId) {
-        boolean existsBySlugAndSeriesIdNot = i18nSeriesRepository.existsBySlugAndSeriesIdNot(slug, seriesId);
-        if (existsBySlugAndSeriesIdNot){
+    private void validateSlug(String slug) {
+        if (i18nSeriesRepository.existsBySlug(slug)) {
             throw new SlugAlreadyExistsException(slug);
         }
+    }
+
+    private void validateDuplicateSlugs(Collection<SeriesLanguageDTO> languages) {
+        Set<String> slugSet = new HashSet<>();
+        for (SeriesLanguageDTO language : languages) {
+            String slug = language.slug();
+            if (!slugSet.add(slug)) {
+                throw new DuplicateSlugInLanguagesException(slug);
+            }
+        }
+    }
+
+    private void validateSlugInCollection(Collection<String> slugs) {
+        slugs.forEach(this::validateSlug);
+    }
+
+    private void validateSlugWithOwner(String slug, UUID seriesId) {
+        if (i18nSeriesRepository.existsBySlugAndSeriesIdNot(slug, seriesId)) {
+            throw new SlugAlreadyExistsException(slug);
+        }
+    }
+
+    /**
+     * @param slugs   the slugs to be validated
+     * @param seriesId the series id to be validated
+     */
+    private void validateSlugWithOwnerInCollection(Collection<String> slugs, UUID seriesId) {
+        slugs.forEach(slug -> validateSlugWithOwner(slug, seriesId));
     }
 
 }
