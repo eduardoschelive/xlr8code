6fa4878c716781eafa10317e70dfa7e1f3cd60b3
Eduardo Schelive <eduardoschelive@gmail.com>
2024-03-19 11:26:12 -0300

    ✨ feat(common): create AuditableEntity class
    this class will be used when the entity has updatedAt and createdAt fields

---
+++ server/src/main/java/com/xlr8code/server/common/entity/AuditableEntity.java
@@ -0,0 +1,26 @@
+package com.xlr8code.server.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class AuditableEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    protected Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", updatable = false)
+    protected Instant updatedAt;
+
+}
