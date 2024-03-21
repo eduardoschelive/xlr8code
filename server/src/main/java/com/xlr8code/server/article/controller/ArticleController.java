eaab95250d95282bb147bc603a6c8d66a74bab64
Eduardo Schelive <eduardoschelive@gmail.com>
2024-03-21 17:57:12 -0300

    ✨ feat(article): create article controller

--- server/src/main/java/com/xlr8code/server/article/controller/ArticleController.java
+++ server/src/main/java/com/xlr8code/server/article/controller/ArticleController.java
@@ -1,4 +1,21 @@
 package com.xlr8code.server.article.controller;
 
+import com.xlr8code.server.article.dto.ArticleDTO;
+import com.xlr8code.server.common.utils.Endpoint;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(Endpoint.Article.BASE_PATH)
 public class ArticleController {
+
+    @PostMapping
+    public void create(@Valid @RequestBody  ArticleDTO articleDTO) {
+        // TODO: implement
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
 }
