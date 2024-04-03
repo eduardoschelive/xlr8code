906d0443f3e90b90fbb7a4c9032fa8ae8eb5d96e
Eduardo Schelive <eduardoschelive@gmail.com>
2024-04-03 16:03:06 -0300

    ✨ feat(common): configure mail sending via smtp

--- server/src/main/java/com/xlr8code/server/ServerApplication.java
+++ server/src/main/java/com/xlr8code/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.xlr8code.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
--- server/src/main/java/com/xlr8code/server/common/service/EmailService.java
+++ server/src/main/java/com/xlr8code/server/common/service/EmailService.java
@@ -1,15 +1,35 @@
 package com.xlr8code.server.common.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
+@RequiredArgsConstructor
 public class EmailService {
 
+    private final JavaMailSender javaMailSender;
+
     public void sendEmail(String email, String subject, String body) {
-        // TODO: Implement email sending and remove logging
-        System.out.println();
-        System.out.println("Email sent to " + email + " with subject " + subject + " and body " + body);
-        System.out.println();
+        log.info("Sending email to {} with subject: {} and body: {}", email, subject, body);
+
+        try {
+            var message = javaMailSender.createMimeMessage();
+
+            var helper = new MimeMessageHelper(message, true);
+            helper.setTo(email);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            helper.setFrom("mail@xlr8code.com");
+
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            log.error("Error while sending email" );
+            e.printStackTrace();
+        }
     }
 
     public void sendActivationEmail(String email, String activationCode) {
